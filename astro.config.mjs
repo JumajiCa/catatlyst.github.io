3576ca66165af2922a94b5663efda9da95db4aae
JumajiCa <AmenYiHonduras@Gmail.Com>
2026-02-21 15:27:12 -0800

    GH Pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://your-username.github.io',
-  base: '/catalyst_website',
+  site: 'https://catalyst.github.io',
+  base: '/catalyst.github.io',
   output: 'static',
   integrations: [react()],
 });
